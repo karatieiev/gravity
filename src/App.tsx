aea29f2fa22ab5c3bb380536c6d59802f39a47c0
Git user <hlamina@ukr.net>
2022-02-08 23:53:59 +0200

    refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import React, {FC} from "react";
-import {Grid} from "@mui/material";
-import {MaterialPointCard} from "./components/MaterialPointCard/MaterialPointCard";
-import {MaterialPoint} from "./helpers/types";
-
-const p: MaterialPoint = {
-    id: 'id',
-    name: 'test',
-    mass: 100000,
-    prevVector: {
-        azimuth: 180.1234567,
-        value: 100,
-        point: {x: 0, y: 0}
-    },
-    nextVector: {
-        azimuth: 0,
-        value: 0,
-        point: {x: 0, y: 0}
-    }
-}
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import {ControlPanel} from "./components/ControlPanel";
+import {MaterialPointsList} from "./components/MaterialPointsList";
 
 export const App: FC = () => {
 
     return (
         <Grid container spacing={0}>
             <Grid item xs={1.5}>
-                <MaterialPointCard point={p} disabled={false} onChange={()=>{}} onDelete={()=>{}} />
+                <Box sx={{mb: 1}}>
+                    <ControlPanel />
+                </Box>
+                <MaterialPointsList />
             </Grid>
             <Grid item xs={10.5}>
                 <div></div>
