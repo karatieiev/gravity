550ebd245099cda2b422793288fcee81e3b6e8a0
Git user <hlamina@ukr.net>
2022-02-08 23:52:53 +0200

    add processing state and refactor addPoint function

--- src/context/MainContextProvider.tsx
+++ src/context/MainContextProvider.tsx
@@ -1,12 +1,15 @@
-import React, {createContext, useState, useContext, FC} from "react";
+import React, {createContext, useState, useContext, FC, useRef} from "react";
 import {MaterialPoint} from "../helpers/types";
+import cloneDeep from "lodash/cloneDeep";
 
 interface ContextProps {
     materialPoints: MaterialPoint[],
     addPoint: Function,
     removePoint: Function,
     replacePoints: Function,
-    updatePoint: Function
+    updatePoint: Function,
+    isProcessing: boolean,
+    setProcessing: Function
 }
 
 const MainContext = createContext<ContextProps>({
@@ -14,14 +17,25 @@ const MainContext = createContext<ContextProps>({
     addPoint: () => {},
     removePoint: () => {},
     replacePoints: () => {},
-    updatePoint: () => {}
+    updatePoint: () => {},
+    isProcessing: false,
+    setProcessing: () => {}
 });
 
 export const MainContextProvider: FC = ({children}) => {
     const [materialPoints, setPoints] = useState<MaterialPoint[]>([]);
+    const [isProcessing, setProcessing] = useState<boolean>(false);
+    const nameNumber = useRef<number>(0);
 
     const addPoint = (point: MaterialPoint) => {
-        setPoints(prevState => [...prevState, point]);
+        let p = point;
+
+        if (!point.name) {
+            p = cloneDeep(point);
+            p.name = `P${nameNumber.current}`;
+            nameNumber.current++;
+        }
+        setPoints(prevState => [...prevState, p]);
     }
 
     const removePoint = (point: MaterialPoint) => {
@@ -33,12 +47,18 @@ export const MainContextProvider: FC = ({children}) => {
     }
 
     const updatePoint = (point: MaterialPoint) => {
-        setPoints(prevState => [...prevState.filter(item => item.id !== point.id), point]);
+        setPoints(prevState => prevState.map(item => item.id === point.id ? point : item));
     }
 
     return (
         <MainContext.Provider value={{
-            materialPoints, addPoint, removePoint, replacePoints, updatePoint
+            materialPoints,
+            addPoint,
+            removePoint,
+            replacePoints,
+            updatePoint,
+            isProcessing,
+            setProcessing
         }}>
             {children}
         </MainContext.Provider>
